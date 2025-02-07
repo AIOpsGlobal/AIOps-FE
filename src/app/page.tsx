2431f68a88d56e01f8f07263ec2cb9ebbb7ab458
Ovodo <ovdizzle@gmail.com>
2025-02-07 13:42:52 +0000

    fixed mutliple metadata exports and renamed opengraph-image properly.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
 // src/app/page.tsx
 
 import Login from "@/components/Login";
-import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
 import Image from "next/image";
 
-export const metadata: Metadata = {
-  title: "AIOps",
-  description: "Rebranding Enterprise solutions",
-};
 
 export default function Home() {
   return (
     <div className="relative flex h-screen w-full items-center justify-center bg-[#B2BAD6] ">
-      {/* <div className="absolute h-[37vw] w-[35vw]  rounded-full"> */}
+      {/* <div className="absolute bord h-[52vw] w-[48vw]"> */}
       <Image
         src={"/images/login_splash.png"}
         alt="Logo"
         fill
-        className="absolute object-cover"
+        className="absolute"
       />
       {/* </div> */}
       <div className="relative flex items-center justify-center rounded-[16px] bg-white p-10 xsm:p-12">
